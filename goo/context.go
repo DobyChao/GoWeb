140695e50cb80dfdba93d0334e9d5c5ba9e5d1ed
DobyChao <106363380+DobyChao@users.noreply.github.com>
2024-01-31 23:53:19 +0800

    finish step 6

--- goo/context.go
+++ goo/context.go
@@ -22,6 +22,8 @@ type Context struct {
 	// middleware
 	handlers []HandlerFunc
 	index    int
+	// engine pointer
+	engine *Engine
 }
 
 // newContext is the constructor of Context
@@ -100,8 +102,10 @@ func (c *Context) Data(code int, data []byte) {
 }
 
 // HTML is the method of Context to write html response
-func (c *Context) HTML(code int, html string) {
+func (c *Context) HTML(code int, name string, data interface{}) {
 	c.SetHeader("Content-Type", "text/html")
 	c.Status(code)
-	c.Writer.Write([]byte(html))
+	if err := c.engine.htmlTemplates.ExecuteTemplate(c.Writer, name, data); err != nil {
+		c.Fail(500, err.Error())
+	}
 }
--- main.go
+++ main.go
@@ -1,58 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"goo"
-	"log"
+	"html/template"
 	"net/http"
 	"time"
 )
 
-func onlyForV2() goo.HandlerFunc {
-	return func(c *goo.Context) {
-		// Start timer
-		t := time.Now()
-		// if a server error occurred
-		c.Fail(500, "Internal Server Error")
-		// Calculate resolution time
-		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
-	}
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 
 func main() {
 	r := goo.New()
-	// r.GET("/index", func(c *goo.Context) {
-	// 	c.HTML(http.StatusOK, "<h1>Index Page</h1>")
-	// })
+	r.Use(goo.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
 
-	// v1 := r.Group("/v1")
-	// v1.GET("/", func(c *goo.Context) {
-	// 	c.HTML(http.StatusOK, "<h1>Hello Goo</h1>")
-	// })
-	// v1.GET("/hello", func(c *goo.Context) {
-	// 	// expect /hello?name=goo
-	// 	c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	// })
-
-	r.Use(goo.Logger()) // global midlleware
+	stu1 := &student{Name: "Geektutu", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
 	r.GET("/", func(c *goo.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		c.HTML(http.StatusOK, "css.tmpl", nil)
 	})
 
-	v2 := r.Group("/v2")
-
-	// use middleware
-	v2.Use(onlyForV2())
-
-	v2.GET("/hello/:name", func(c *goo.Context) {
-		// expect /hello/goo
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	r.GET("/students", func(c *goo.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", goo.H{
+			"title":  "gee",
+			"stuArr": [2]*student{stu1, stu2},
+		})
 	})
-	// v2.POST("/login", func(c *goo.Context) {
-	// 	c.JSON(http.StatusOK, goo.H{
-	// 		"username": c.PostForm("username"),
-	// 		"password": c.PostForm("password"),
-	// 	})
-	// })
+
+	r.GET("/date", func(c *goo.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", goo.H{
+			"title": "gee",
+			"now":   time.Date(2024, 1, 31, 0, 0, 0, 0, time.UTC),
+		})
+	})
 
 	r.Run(":9999")
 }
